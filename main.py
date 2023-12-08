dd4691fdca80055ee5b0477910c6f6c94cc8c3be
Mohan5353 <120332828+Mohan5353@users.noreply.github.com>
2023-12-08 18:29:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import flet as ft
 
-from datetime import date
-from json import dump, load
+# from datetime import date
+# from json import dump, load
 
 from requests import get
 
 
-def get_response(*args) -> dict:
-    with open('Currency_Rate.json') as f:
-        return load(f)
+# def get_response(*args) -> dict:
+#     with open('Currency_Rate.json') as f:
+#         return load(f)
 
 
 def update(*args: object) -> dict:
-    present = date.today().strftime("%d %b %Y")
-    response = get_response()
-    file_date = response["time_last_update_utc"][5:16]
-    # if present != file_date:
-    #     f = open('Currency_Rate.json', 'w')
-    #     url = "https://v6.exchangerate-api.com/v6/a04cc39aea88ee70a55c180c/latest/USD"
-    #     response = get(url).json()
-    #     dump(response, f, indent=4)
-    #     f.close()
+    url = "https://v6.exchangerate-api.com/v6/a04cc39aea88ee70a55c180c/latest/USD"
+    response = get(url).json()
     response = response['conversion_rates']
     return response
 
